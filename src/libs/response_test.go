becf5ecc85557be86a64a506dc6cb823c31f94f3
Muhamad Wildan Faz <muhamadwildanfaz@gmail.com>
2022-09-29 00:15:01 +0700

    Vehicle Rental - heroku

--- src/libs/response_test.go
+++ src/libs/response_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestResponse(t *testing.T) {
 	result := Response(nil, 200, "success", nil)
 
-	if result.IsError != nil {
-		t.Fatal("error :", result.IsError)
+	if result.Error != nil {
+		t.Fatal("error :", result.Error)
 	}
 }
