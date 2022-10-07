ca97102fce9b0b33f88f88133345597974760a88
Muhamad Wildan Faz <muhamadwildanfaz@gmail.com>
2022-10-07 17:48:51 +0700

    Vehicle Rental - heroku

--- src/libs/response.go
+++ src/libs/response.go
@@ -14,9 +14,9 @@ type Resp struct {
 }
 
 func (res *Resp) Send(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000/")
-	// w.Header().Set("Access-Control-Allow-Methods", "*")
-	// w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Content-Type", "application/json")
 
 	w.WriteHeader(res.Status)
