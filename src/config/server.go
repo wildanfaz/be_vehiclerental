dec41d8937f091b9cac2f544d9c506d5c3484c96
Muhamad Wildan Faz <muhamadwildanfaz@gmail.com>
2022-10-16 03:38:23 +0700

    Vehicle Rental - heroku

--- src/config/server.go
+++ src/config/server.go
@@ -22,7 +22,7 @@ func server(cmd *cobra.Command, args []string) error {
 		c := cors.New(cors.Options{
 			AllowedOrigins: []string{"*"},
 			AllowedHeaders: []string{"*"},
-			AllowCredentials: false,
+			AllowCredentials: true,
 			// Enable Debugging for testing, consider disabling in production
 			Debug: false,
 		})
