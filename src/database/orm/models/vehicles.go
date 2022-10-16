779a2a89845e4ef2b429fc6e6a094065d17adcb2
Muhamad Wildan Faz <muhamadwildanfaz@gmail.com>
2022-10-16 20:58:18 +0700

    Vehicle Rental - heroku

--- src/database/orm/models/vehicles.go
+++ src/database/orm/models/vehicles.go
@@ -10,7 +10,7 @@ type Vehicle struct {
 	VehicleName string    `json:"vehiclename,omitempty" form:"vehiclename"`
 	Location    string    `json:"location,omitempty" form:"location"`
 	Description string    `json:"description,omitempty" form:"description"`
-	Price       int       `json:"price,string,omitempty" form:"price"`
+	Price       json.Number       `json:"price,omitempty" form:"price"`
 	Status      string    `json:"status,omitempty" form:"status"`
 	Stock       int       `json:"stock,string,omitempty" form:"stock"`
 	Category    string    `json:"category,omitempty" form:"category"`
